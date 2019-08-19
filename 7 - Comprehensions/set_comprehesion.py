c2a6641d023c0d51c723363efcf36bcf653c18d2
Lucas Barros Ribeiro <lucas.barros@sicoob.com.br>
2019-08-19 10:36:34 -0300

    att

--- 7 - Comprehensions/set_comprehesion.py
+++ 7 - Comprehensions/set_comprehesion.py
@@ -10,7 +10,7 @@ print(numeros) # {1, 2, 3, 4, 5}
 numeros = {x * 2 for x in range(6)}
 print(numeros) # -> {0, 2, 4, 6, 8, 10}
 
-# Desafio: Faça uma alteração na estrutura acima para gerar um dicionario ao invéis de set
+# Desafio: Faça uma alteração na estrutura acima para gerar um dicionario ao invés de set
 numeros = {x: x * 2 for x in range(6)}
 print(numeros) # -> {0: 0, 1: 2, 2: 4, 3: 6, 4: 8, 5: 10}
 
---
+++ 8 - Expressoes Lambdas e Funcoes Integradas/lambdas.py
@@ -0,0 +1,20 @@
+# Lambdas
+
+# Conhecidas por Expressões Lambdas, ou simplesmente Lambdas, são funções sem nome, ou seja, funções anônimas.
+
+# Função em Python
+
+def soma(a, b): # exemplo 1
+    return a + b
+
+def funcao(x): # exemplo 2
+    return 3 * x + 1
+
+# print(funcao(4)) # ->
+
+# Exemplo de Expressão Lambda
+lambda x: 3 * x + 1
+
+# E como utilizar expressões lambdas?
+calc = lambda x: 3 * x + 1
+# print(calc(4)) -> 13
